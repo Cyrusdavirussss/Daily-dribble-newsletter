c776e0822717d6a9609db0b0b33fb943b06fadef
Cyrusdavirussss <87535794+Cyrusdavirussss@users.noreply.github.com>
2024-12-10 11:27:47 +0000

    Add files via upload

---
+++ styles.css
@@ -0,0 +1,83 @@
+
+/* Main container with margin-top */
+.main {
+    margin-top: 30px;
+ 
+}
+
+/* Heading styles */
+h1 {
+    text-align: center;
+    color: #121212;
+}
+
+/* Styling for the image */
+.logo-img {
+    display: block;
+    width: 380px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 3px solid #121212;
+}
+
+/* Description styling (applies to the h4 inside .description) */
+.description {
+    max-width: 380px;
+    margin: 0 auto;
+    text-align: center;
+    font-style: italic;
+    font-weight: normal;
+    
+}
+
+.terms-condition {
+    text-align: center; /* Center-align the text of the entire paragraph */
+}
+
+
+.terms-condition a {
+    color: black; /* Set link color to black */
+    cursor: default; /* Prevent pointer cursor */
+    text-decoration:underline /* Remove underline */
+}
+
+/* Styling for email input field */
+.email-input {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    border: 3px solid #121212;
+    padding: 10px 20px 10px 30px;
+    margin-bottom: 10px;
+    margin-top: 40px;
+    width: 221px;
+    box-sizing: border-box;
+    height: 40px;
+}
+
+input::placeholder{
+    color: grey;
+    font-weight: bold;
+}
+
+/* Button styling (if you have a .btn class) */
+.btn {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 10px 25px 10px 25px;
+    background-color: #121212;
+    color: white;
+    border: none;
+    cursor: pointer;
+    text-align: center;
+    font-size: 16px;
+    width: 221px;
+    box-sizing: border-box;
+    height: 40px;
+    
+}
+
+.btn:hover {
+    background-color: #333;
+}
